42601d6e320dce29b5db78d781b5ddd6e3bac0ac
Kevin <kevinmanueldiazluis@gmail.com>
2025-04-07 12:25:15 -0700

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+export default {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
+// Copiamos esto desde una version antigua de la domentacion de la pagina de tailwind
